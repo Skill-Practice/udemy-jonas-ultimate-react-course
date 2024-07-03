9d0954b13a3f17b6b88135f48c637db6a410b225
Billy Wang <billy.local>
2024-07-03 15:42:19 +0800

    [Section 4: [Optional] Review of Essential JavaScript for React]: Working With Immutable Arrays

--- practice/JS-review/starter/script.js
+++ practice/JS-review/starter/script.js
@@ -281,10 +281,29 @@ books;
 // const pagesAllBooks = books.reduce((sum, book) => sum + book.pages, 0);
 // pagesAllBooks;
 
-const arr = [3, 7, 1, 9, 6];
-const sorted = arr.slice().sort((a, b) => a - b);
-sorted;
-arr;
+// const arr = [3, 7, 1, 9, 6];
+// const sorted = arr.slice().sort((a, b) => a - b);
+// sorted;
+// arr;
 
-const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
-sortedByPages;
+// const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
+// sortedByPages;
+
+// 1) Add book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J. K. Rowling",
+};
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2) Delete book object from array
+const booksAfterAddDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterAddDelete;
+
+// 3) Update book object in the array
+const booksAfterUpdate = booksAfterAddDelete.map((book) =>
+  book.id === 1 ? { ...book, pages: 1 } : book
+);
+booksAfterUpdate;
