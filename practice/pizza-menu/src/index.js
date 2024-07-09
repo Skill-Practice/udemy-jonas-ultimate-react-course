6e0fd7a47eb1afd5ba30470925033c5f1dc90e2d
Billy Wang <billy.local>
2024-07-09 22:36:24 +0800

    [Section 5: Working With Components, Props, and JSX]: Destructuring Props

--- practice/pizza-menu/src/index.js
+++ practice/pizza-menu/src/index.js
@@ -124,7 +124,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHours={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -148,11 +148,11 @@ function Footer() {
   // return React.createElement("footer", null, "We're currently open!");
 }
 
-function Order(props) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <p>
-        We're open until until {props.closeHours}:00. Come visit us or order
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
         online.
       </p>
       <button className="btn">Order</button>
@@ -160,16 +160,17 @@ function Order(props) {
   );
 }
 
-function Pizza(props) {
-  console.log(props);
+// function Pizza(props) {
+function Pizza({ pizzaObj }) {
+  // console.log(props);
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
       </div>
     </li>
   );
