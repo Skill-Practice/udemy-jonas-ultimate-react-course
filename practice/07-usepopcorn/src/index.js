dcc90c7ebf46ffb18866827a5a63c46d1d7c4815
Billy Wang <billy.local>
2024-07-22 16:09:08 +0800

    [Section 10: Thinking in React: Components, Composition, and Reusability]: PropTypes

--- practice/07-usepopcorn/src/index.js
+++ practice/07-usepopcorn/src/index.js
@@ -21,7 +21,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating
-      maxRating={5}
+      maxRating={"bbb"}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
