347b74f36e61759e7961cf619c3d11f41fec24be
Billy Wang <billy.local>
2024-07-30 19:14:47 +0800

    [Section 14: [Optional] React Before Hooks: Class-Based React]: Child to Parent Communication

--- practice/09-classy-weather/src/App.js
+++ practice/09-classy-weather/src/App.js
@@ -88,16 +88,16 @@ class App extends React.Component {
     }
   };
 
+  setLocation = (e) => this.setState({ location: e.target.value });
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
         <div>
-          <input
-            type="text"
-            placeholder="Search from location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
+          <Input
+            location={this.state.location}
+            onChangeLocation={this.setLocation}
           />
         </div>
         <button onClick={this.fetchWeather}>Get weather</button>
@@ -116,6 +116,21 @@ class App extends React.Component {
 }
 
 export default App;
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search from location..."
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
+      </div>
+    );
+  }
+}
 
 class Weather extends React.Component {
   render() {
