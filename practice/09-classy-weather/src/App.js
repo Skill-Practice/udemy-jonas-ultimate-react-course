c0daeb9cbe179f1f12df521ff7a9a4f8c431d503
Billy Wang <billy.local>
2024-07-30 18:09:47 +0800

    [Section 14: [Optional] React Before Hooks: Class-Based React]: Working With Event Handlers

--- practice/09-classy-weather/src/App.js
+++ practice/09-classy-weather/src/App.js
@@ -5,14 +5,36 @@ class Counter extends React.Component {
     super(props);
 
     this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    console.log(this);
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+
+    // this.setState({ count: 10 });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} {this.state.count}
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
