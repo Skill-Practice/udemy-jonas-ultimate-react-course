35b760d9d8efa675a9a229898ea9ff473c6ba701
Billy Wang <billy.local>
2024-07-30 13:41:32 +0800

    [Section 12: Effects and Data Fetching]: Creating useLocalStorageState

---
+++ practice/07-usepopcorn/src/useLocalStorageState.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const [value, setValue] = useState(function () {
+    const storeedValue = localStorage.getItem(key);
+    return storeedValue ? JSON.parse(storeedValue) : initialState;
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
+
+  return [value, setValue];
+}
