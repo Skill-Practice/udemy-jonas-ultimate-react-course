558e536c79ae2c9f83d5b67de3d5b2a70a0e9ea2
Billy Wang <billy.local>
2024-07-31 15:35:47 +0800

    [Section 17: React Router: Building Single-Page Applications (SPA)]: Building the Pages

--- practice/11-worldwise/src/App.jsx
+++ practice/11-worldwise/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
-import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+// import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        {/* <Route path="app" element={<AppLayout />}></Route> */}
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
