c4cc5c666d430bce72f9be4e7104db1d2df4ea44
Billy Wang <billy.local>
2024-07-31 22:45:33 +0800

    [Section 17: React Router: Building Single-Page Applications (SPA)]: Programmatic Navigation with <Navigate />

--- practice/11-worldwise/src/App.jsx
+++ practice/11-worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -45,7 +45,8 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<Navigate replace to="cities" />}
+            // element={<CityList cities={cities} isLoading={isLoading} />}
           />
           <Route
             path="cities"
