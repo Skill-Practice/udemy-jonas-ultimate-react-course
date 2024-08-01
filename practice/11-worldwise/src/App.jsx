d7befdee66e94ce59afb0e869d78ed5c300ae30b
Billy Wang <billy.local>
2024-08-02 00:37:11 +0800

    [Section 18: Advanced State Management: The Context API]: Adding Fake Authentication: Implementing "Login"

--- practice/11-worldwise/src/App.jsx
+++ practice/11-worldwise/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Product from "./pages/Product";
@@ -12,39 +11,42 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProdiver } from "./contexts/FakeAuthContext";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />}></Route>
-          <Route path="product" element={<Product />}></Route>
-          <Route path="pricing" element={<Pricing />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              element={<Navigate replace to="cities" />}
-              // element={<CityList cities={cities} isLoading={isLoading} />}
-            />
-            <Route
-              path="cities"
-              element={<CityList />}
-              // element={<CityList cities={cities} isLoading={isLoading} />}
-            />
-            <Route path="cities/:id" element={<City />} />
-            <Route
-              path="countries"
-              element={<CountryList />}
-              // element={<CountryList cities={cities} isLoading={isLoading} />}
-            />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProdiver>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />}></Route>
+            <Route path="product" element={<Product />}></Route>
+            <Route path="pricing" element={<Pricing />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="app" element={<AppLayout />}>
+              <Route
+                index
+                element={<Navigate replace to="cities" />}
+                // element={<CityList cities={cities} isLoading={isLoading} />}
+              />
+              <Route
+                path="cities"
+                element={<CityList />}
+                // element={<CityList cities={cities} isLoading={isLoading} />}
+              />
+              <Route path="cities/:id" element={<City />} />
+              <Route
+                path="countries"
+                element={<CountryList />}
+                // element={<CountryList cities={cities} isLoading={isLoading} />}
+              />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProdiver>
   );
 }
 
