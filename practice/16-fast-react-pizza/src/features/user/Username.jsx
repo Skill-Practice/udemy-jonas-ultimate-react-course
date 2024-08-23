a38d4190fc623be953140966476b6965dcc637fb
Billy Wang <billy.local>
2024-08-24 00:11:34 +0800

    [Section 23: [Optional] Tailwind CSS Crash Course: Styling the App]: The Box Model: Spacing, Borders, and Display

--- practice/16-fast-react-pizza/src/features/user/Username.jsx
+++ practice/16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Billy</div>;
+  return <div className="hidden text-sm font-semibold">Billy</div>;
 }
 
 export default Username;
