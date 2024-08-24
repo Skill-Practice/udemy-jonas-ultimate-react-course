7d3bec7823ef26a4679324fbb76cfcac9be39ec4
Billy Wang <billy.local>
2024-08-24 15:17:52 +0800

    [Section 23: [Optional] Tailwind CSS Crash Course: Styling the App]: Using Flexbox

--- practice/16-fast-react-pizza/src/features/user/Username.jsx
+++ practice/16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Billy</div>;
+  return <div className="hidden text-sm font-semibold md:block">Billy</div>;
 }
 
 export default Username;
