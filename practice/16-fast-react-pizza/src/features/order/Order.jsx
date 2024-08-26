89067abe6bc5bb8e2309d9c0f574d67491c1bf85
Billy Wang <billy.local>
2024-08-26 12:20:19 +0800

    [Section 24: Adding Redux and Advanced React Router]: Fetching Data Without Navigation: useFetcher

--- practice/16-fast-react-pizza/src/features/order/Order.jsx
+++ practice/16-fast-react-pizza/src/features/order/Order.jsx
@@ -1,14 +1,15 @@
 // Test ID: IIDSAT
+import { useFetcher, useLoaderData } from 'react-router-dom';
 
 import OrderItem from './OrderItem';
 
-import { useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from '../../utils/helpers';
+import { useEffect } from 'react';
 
 // const order = {
 //   id: "ABCDEF",
@@ -47,6 +48,17 @@ import {
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
+
+  console.log(fetcher.data);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -90,7 +102,15 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
