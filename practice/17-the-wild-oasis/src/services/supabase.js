3b789fb95a039b0c57d92da1859b28ba28f200e7
Billy Wang <billy.local>
2024-08-29 14:25:40 +0800

    [Section 25: Setting Up Our Biggest Project + Styled Components]: Connecting Supabase With Our React App

---
+++ practice/17-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://lwcnaxjpnmtwubcbdyuk.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3Y25heGpwbm10d3ViY2JkeXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4NjEzNTIsImV4cCI6MjA0MDQzNzM1Mn0.KVSjUpwv6wzQbmXStRpFUdmugNLdQllmSc35bO6lsmE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
