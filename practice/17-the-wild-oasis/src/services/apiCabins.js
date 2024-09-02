0d007dbdb94b97c6f0619631a8b524e99dfad079
Billy Wang <billy.local>
2024-09-02 19:47:51 +0800

    [Section 27: React Query: Managing Remote State]: Creating a New Cabin

--- practice/17-the-wild-oasis/src/services/apiCabins.js
+++ practice/17-the-wild-oasis/src/services/apiCabins.js
@@ -11,8 +11,26 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  // const { data, error } = await supabase
+  // .from('cabins')
+  // .insert([
+  //   { some_column: 'someValue', other_column: 'otherValue' },
+  // ])
+  // .select()
+
+  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be crated");
+  }
+
+  return data;
+}
+
 export async function deleteCabin(id) {
-  const { data, error } = await supabase.from("cabins").delete().eq("id" , id);
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
   if (error) {
     console.log(error);
