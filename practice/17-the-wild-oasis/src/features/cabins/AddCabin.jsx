6549d8c9d70a7490e467acca2d20dba741cc4842
Billy Wang <billy.local>
2024-09-04 20:23:35 +0800

    [Section 28: Advanced React Patterns]: Converting the Modal to a Compound Component

--- practice/17-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ practice/17-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,24 +1,44 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
-  const [isOpenModal, setOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
 
-      {isOpenModal && (
-        <Modal onClose={() => setOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setOpenModal((show) => !show)}>
+//         Add new cabin
+//       </Button>
+
+//       {isOpenModal && (
+//         <Modal onClose={() => setOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
