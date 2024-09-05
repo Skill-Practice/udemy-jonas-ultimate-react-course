f7670412833857631aac971865cdcac550aae5a6
Billy Wang <billy.local>
2024-09-06 00:29:12 +0800

    [Section 29: [Optional] Implementing More Features: Authentication, Dark Mode, Dashboard, etc]:  Client-Side Sorting: Sorting Cabins

--- practice/17-the-wild-oasis/src/ui/Filter.jsx
+++ practice/17-the-wild-oasis/src/ui/Filter.jsx
@@ -51,10 +51,11 @@ function Filter({ filterField, options }) {
           key={option.value}
           onClick={() => handleClick(option.value)}
           $active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
         >
           {option.label}
         </FilterButton>
-      ))}
+      ))} 
     </StyledFilter>
   );
 }
