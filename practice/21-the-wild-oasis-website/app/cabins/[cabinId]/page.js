086afc64906a3c95a414f0d88fbd22181d8fe812
Billy Wang <billy.local>
2024-09-16 19:19:09 +0800

    [Section 34: Data Fetching, Caching, and Rendering]: Making Dynamic Pages Static With generateStaticParams

--- practice/21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ practice/21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 
 // PLACEHOLDER DATA
@@ -23,6 +23,16 @@ export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
 
   return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
