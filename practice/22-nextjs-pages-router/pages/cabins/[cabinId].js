cdfd2a0f30f5705184c2c083a64bc80f3c6df7af
Billy Wang <billy.local>
2024-09-22 13:14:42 +0800

    [Section 39: [OPTIONAL] Legacy Next.js The “Pages” Router]: Defining Page Title and Favicon

--- practice/22-nextjs-pages-router/pages/cabins/[cabinId].js
+++ practice/22-nextjs-pages-router/pages/cabins/[cabinId].js
@@ -1,9 +1,18 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 function Cabin() {
   const router = useRouter();
 
-  return <div>Cabin #{router.query.cabinId}</div>;
+  return (
+    <>
+      <Head>
+        <title>Cabin #{router.query.cabinId} // The WIld Oasis</title>
+      </Head>
+
+      <div>Cabin #{router.query.cabinId}</div>
+    </>
+  );
 }
 
 export default Cabin;
