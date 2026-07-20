24c925bc62de83221d5b9475794825d4559176a2
billy <billy@billydeMacBook-Pro.local>
2026-07-20 12:42:20 +0800

    chore(practice/17-the-wild-oasis/src/services/supabase.js): update supabase credentials

--- practice/17-the-wild-oasis/src/services/supabase.js
+++ practice/17-the-wild-oasis/src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://lwcnaxjpnmtwubcbdyuk.supabase.co";
+export const supabaseUrl = "https://cixdgmsbijyrclbibwar.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3Y25heGpwbm10d3ViY2JkeXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4NjEzNTIsImV4cCI6MjA0MDQzNzM1Mn0.KVSjUpwv6wzQbmXStRpFUdmugNLdQllmSc35bO6lsmE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpeGRnbXNiaWp5cmNsYmlid2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0ODc2MjIsImV4cCI6MjEwMDA2MzYyMn0.He98eBzqSXE8EoT0ukoV5Zwnq73EM4m3DE0r2IaVvMM";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
